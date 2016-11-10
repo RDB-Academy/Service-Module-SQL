c82a1bc784cac32c4ff6da149d80865799fc82d1
Fabio Mazzone <fabio.mazzone@me.com>
2016-11-10 17:32:12 +0100

    Fullstack Test

--- app/models/Task.java
+++ app/models/Task.java
@@ -1,5 +1,7 @@
 package models;
 
+import play.data.validation.Constraints;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -11,7 +13,29 @@ public class Task extends BaseModel{
     @Id
     private long id;
 
+    @Constraints.Required
     private String text;
 
+    @Constraints.Required
     private String referenceStatement;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getReferenceStatement() {
+        return referenceStatement;
+    }
+
+    public void setReferenceStatement(String referenceStatement) {
+        this.referenceStatement = referenceStatement;
+    }
 }
--- test/controllers/api/TaskControllerTest.java
+++ test/controllers/api/TaskControllerTest.java
@@ -2,15 +2,20 @@ package controllers.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import play.Application;
+import play.Configuration;
 import play.Mode;
+import play.db.Database;
+import play.db.Databases;
+import play.db.evolutions.Evolutions;
 import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
-import play.test.FakeApplication;
 import play.test.Helpers;
 
 import static junit.framework.TestCase.assertTrue;
@@ -20,17 +25,28 @@ import static play.test.Helpers.route;
 public class TaskControllerTest {
 
     protected Application application;
+    Database database;
 
     @Before
     public void startApp() throws Exception {
-        ClassLoader classLoader = FakeApplication.class.getClassLoader();
-        application = new GuiceApplicationBuilder().in(classLoader)
-                .in(Mode.TEST).build();
+        Config config = ConfigFactory.load("test");
+        Configuration configuration = new Configuration(config);
+        String dbDriver = configuration.getString("db.default.driver");
+        String dbUrl = configuration.getString("db.default.url");
+
+        database = Databases.createFrom(dbDriver, dbUrl);
+        Evolutions.cleanupEvolutions(database);
+        Evolutions.applyEvolutions(database);
+
+        application = new GuiceApplicationBuilder()
+                .in(Mode.TEST)
+                .loadConfig(configuration)
+                .build();
         Helpers.start(application);
     }
 
     @Test
-    public void myPostActionTest() throws Exception {
+    public void TaskControllerCreate() throws Exception {
 
         JsonNode jsonNode = (new ObjectMapper()).readTree("{ \"someName\": \"sameValue\" }");
         Http.RequestBuilder request = new Http.RequestBuilder().method("POST")
@@ -44,6 +60,7 @@ public class TaskControllerTest {
     @After
     public void stopApp() throws Exception {
         Helpers.stop(application);
+        database.shutdown();
     }
 
 }
