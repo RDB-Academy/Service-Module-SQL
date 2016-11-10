fd4f291bd95163b8569a30925da8acbf216da13c
goldenticket <n.naczk@tu-bs.de>
2016-11-10 15:51:43 +0100

    bla3

--- app/controllers/admin/HomeController.java
+++ app/controllers/admin/HomeController.java
@@ -1,18 +1,21 @@
 package controllers.admin;
 
-/**
- * Created by nicolenaczk on 03.11.16.
- */
-public class HomeController {
-
+import form.LoginForm;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.inject.Inject;
+
 /**
- * @author fabiomazzone
+ * Created by nicolenaczk on 03.11.16.
  */
+
 public class HomeController extends Controller {
 
+    @Inject
+    FormFactory formFactory;
     public Result index() {
         // ToDo
         // If is Logged in
@@ -26,6 +29,15 @@ public class HomeController extends Controller {
         // If has session
         // forward to index
         // Else
-        return ok(views.html.login.render());
+        Form<LoginForm> loginForm = formFactory.form(LoginForm.class);
+        return ok(views.html.login.render(loginForm));
+    }
+
+    public Result getLogin() {
+        Form<LoginForm> requestData = formFactory.form(LoginForm.class).bindFromRequest();
+        LoginForm loginForm = requestData.get();
+
+        return ok("Hello " + loginForm);
+
     }
 }
--- app/form/LoginForm.java
+++ app/form/LoginForm.java
@@ -2,24 +2,14 @@ package form;
 
 import play.data.validation.Constraints;
 
-import javax.validation.Constraint;
-
 /**
  * Created by nicolenaczk on 03.11.16.
  */
 public class LoginForm {
 
     @Constraints.Required
-    protected String username;
     protected String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
 
     public void setPassword(String password) {
         this.password = password;
