6bd3be525fa5aac9135072491d095285f23c1577
Fabio Mazzone <fabio.mazzone@me.com>
2016-11-11 23:10:23 +0100

    routing fixes

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,6 +4,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Singleton;
+import java.io.File;
 
 /**
  * @author fabiomazzone
@@ -14,6 +15,10 @@ public class HomeController extends Controller {
         return redirect("/web/");
     }
     public Result webApp(String path) {
+        File file = new File("public/dist/" + path);
+        if (file.exists()) {
+            return ok(file);
+        }
         return ok(new java.io.File("public/dist/index.html"));
     }
     public Result webAppRedirect(String path) {
