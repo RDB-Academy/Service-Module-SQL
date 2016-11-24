6f6f6fc3a3965ac20c04ddec4d49a3f88acb87e1
goldenticket <n.naczk@tu-bs.de>
2016-11-24 18:13:48 +0100

    created tableDefviews and delete function, todo next: redirect on tabledelete, columdefviews, tabledef edit and columndef edit

--- modules/common/app/services/TableDefService.java
+++ modules/common/app/services/TableDefService.java
@@ -1,8 +1,11 @@
 package services;
 
+import models.SchemaDef;
 import models.TableDef;
 import play.data.Form;
+import play.data.FormFactory;
 import repository.TableDefRepository;
+import services.tools.ServiceError;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -13,16 +16,42 @@ import javax.inject.Singleton;
 @Singleton
 public class TableDefService {
     private TableDefRepository tableDefRepository;
+    private FormFactory formFactory;
 
     @Inject
     public TableDefService(
-            TableDefRepository tableDefRepository) {
+            TableDefRepository tableDefRepository, FormFactory formFactory) {
 
         this.tableDefRepository = tableDefRepository;
+        this.formFactory = formFactory;
     }
 
     public Form<TableDef> getViewForm(Long id) {
         TableDef tableDef = this.tableDefRepository.getById(id);
+
+            if (tableDef == null) {
+            return null;
+        }
+
+        Form<TableDef> tableDefForm = this.formFactory.form(TableDef.class).fill(tableDef);
+
+        return tableDefForm;
+    }
+
+    public TableDef getById(Long id) {
+
+        return this.tableDefRepository.getById(id);
+
+    }
+
+    public ServiceError deleteTableDef(Long id) {
+        TableDef tableDef = this.tableDefRepository.getById(id);
+
+        if(tableDef == null) {
+            return new ServiceError(ServiceError.NotFound, "TableDef", id);
+        }
+
+        tableDef.delete();
 
         return null;
     }
