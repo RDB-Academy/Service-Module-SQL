92acbf91800a54bcbb6a3ea05ff7a10f242552f2
Fabio Mazzone <fabio.mazzone@me.com>
2016-12-20 18:40:04 +0100

    some cleanup

--- modules/common/app/models/BaseModel.java
+++ modules/common/app/models/BaseModel.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * @author fabiomazzone
+ * The BaseModel
  */
 @MappedSuperclass
 public abstract class BaseModel extends Model {
