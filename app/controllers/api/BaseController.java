f4dc4c65258206b7a81910848abf5e7307cc02de
soeren van der wall <soeren.vanderwall@web.de>
2017-06-12 17:38:45 +0200

    parameter parsing for database access via API 1st version

--- app/controllers/api/BaseController.java
+++ app/controllers/api/BaseController.java
@@ -1,11 +1,14 @@
 package controllers.api;
 
+import models.BaseModel;
 import models.Session;
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Http;
 import services.SessionService;
 
 import javax.validation.constraints.NotNull;
+import java.util.*;
 
 /**
  * @author Fabio Mazzone
@@ -26,5 +29,47 @@ public abstract class BaseController extends Controller
             return sessionService.findActiveSessionById(sessionId);
         }
         return null;
+    }
+
+    protected Map<String, List<String>> extractParameters(
+            Class model, Map<String, String[]> inputParameters) {
+        if (!BaseModel.class.isAssignableFrom(model)) {
+            Logger.error("Wrong class");
+        }
+
+        Set<String> validKeys = new HashSet<>();
+        Arrays.stream(model.getDeclaredFields()).forEach(field ->
+                validKeys.add(field.getName().toLowerCase()));
+
+        Logger.debug(Arrays.asList(validKeys.toArray()).toString());
+
+        Map<String, List<String>> outputParameters = new HashMap<>();
+        inputParameters.forEach((key, val) -> {
+            if (!validKeys.contains(key)) {
+                return;
+            }
+
+            String command = val[0];
+            if (command.isEmpty()) {
+                return;
+            } else if (command.startsWith("<")) {
+                List<String> targetValues = new ArrayList<>();
+                targetValues.add("lt");
+                targetValues.add(command.substring(1));
+                outputParameters.put(key, targetValues);
+            } else if (command.startsWith(">")) {
+                List<String> targetValues = new ArrayList<>();
+                targetValues.add("gt");
+                targetValues.add(command.substring(1));
+                outputParameters.put(key, targetValues);
+            } else if (command.startsWith("=")) {
+                List<String> targetValues = new ArrayList<>();
+                targetValues.add("eq");
+                targetValues.add(command.substring(1));
+                outputParameters.put(key, targetValues);
+            }
+        });
+
+        return outputParameters;
     }
 }
